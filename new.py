bf8e16369caf220420c83c54ef9f942aa81cf766
Blind_one <ftosims@outlook.com>
2018-07-31 07:57:25 +0800

    new.py现在只进行获取每页中图像链接的操作
    后续会慢慢将功能封装到pixivclass.py中

--- new.py
+++ new.py
@@ -1,38 +1,30 @@
 import pixivcalss
 import re
 import requests
+import time
+import os
+
+##
+# 虽然增加了图像是否相同的判断，但是并没有写重复了多少次之后停止运行
+# 所以需要手动C-c
+
 if __name__ == '__main__':
     a = pixivcalss.pixiv()
     pag = 1
-    sometingwrong = []
+    num = 0
     #循环获取
+    
     while(1):
-        try:
-            qwq = a.session.get(a.info.book_mark + str(pag))
-        except requests.exceptions.RequestException:
-            qwq = a.session.get(a.info.book_mark + str(pag))
-        #正则
-        qaq = re.compile(r'</div><a href="(.*?)" class="work  _work .*?"')
-        next_url = re.findall(qaq,qwq.text)
+        # 获取bookmark页面，并且获得其中的图像页面链接
+        markpage = a.get(a.info.book_mark+ str(pag))
         
+        #正则
+        next_url = a.findnext(a.info.firstRe, markpage.text)
+        num = 0
         for url in next_url:
-            try:
-                target = a.session.get('https://www.pixiv.net/' + url)
-            except requests.exceptions.RequestException:
-                target = a.session.get('https://www.pixiv.net/' + url)
-            qaq = re.compile(r'<img alt="(.*?)".*?data-src="(https://i.pximg.net/img-original/img/.*?_p0.(jpg|png))')
-            imgurl = re.findall(qaq, target.text)
-            if len(imgurl) == 1:
-                imgdata = a.session.get(imgurl[0][1])
-                imgdata = imgdata.content
-                savedir = a.info.save_dir + imgurl[0][0] +'.' +imgurl[0][2]
-                with open(savedir.replace('/',' '),'wb') as f:
-                    f.write(imgdata)
-                print('ok')
-            else:
-                print('next')
-                #多图url逻辑,待写
-        print(next_url)
+            a.getpic('https://www.pixiv.net/' + url)
+            
+        # 如果一个页面里的连接小于20，那么在执行完该页面之后将停止循环
         if (len(next_url) < 20):
             break
         pag+=1
